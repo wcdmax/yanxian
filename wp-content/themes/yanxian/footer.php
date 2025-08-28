06ace600d8c47cacd45b492e252df0bafa04a518
Kevin <wcdma020@hotmail.com>
2025-08-28 16:29:32 +0800

    chore: 完成页脚部分，添加公司信息和导航链接

--- wp-content/themes/yanxian/footer.php
+++ wp-content/themes/yanxian/footer.php
@@ -1,2 +1,115 @@
+<footer class="uk-section uk-section-small uk-background-secondary">
+    <div class="uk-container uk-container-large">
+        <div uk-grid class="uk-grid-small uk-grid-match">
+            <div class="uk-width-1-3">
+                <ul class="uk-list yx-footer-list">
+                    <li class="yx-footer-item">
+                        <p>销售热线</p>
+                        <h4>19925438691</h4>
+                    </li>
+                    <li class="yx-footer-item">
+                        <p>总机号码</p>
+                        <h4>400-8898-583</h4>
+                    </li>
+                    <li class="yx-footer-item">
+                        <p>邮箱地址</p>
+                        <h4>service@yxtouch.com</h4>
+                    </li>
+                    <li class="yx-footer-item">
+                        <p>公司地址</p>
+                        <p>深圳市宝安区沙井街道大王山社区大王山第三工业区A1栋六层</p>
+                    </li>
+                </ul>
+            </div>
+            <div class="uk-width-2-3">
+                <div uk-grid class="uk-grid-small uk-grid-match uk-child-width-1-5">
+                    <div>
+                        <ul class="uk-list yx-footer-list">
+                            <li class="yx-footer-item">
+                                <h4 class="uk-footer-title">关于我们</h4>
+                            </li>
+                            <li class="yx-footer-item">
+                                <a href="#" target="_blank" class="uk-text-decoration-none">公司简介</a>
+                            </li>
+                            <li class="yx-footer-item">
+                                <a href="#" target="_blank" class="uk-text-decoration-none">联系我们</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul class="uk-list yx-footer-list">
+                            <li class="yx-footer-item">
+                                <h4 class="uk-footer-title">新闻资讯</h4>
+                            </li>
+                            <li class="yx-footer-item">
+                                <a href="#" target="_blank" class="uk-text-decoration-none">企业动态</a>
+                            </li>
+                            <li class="yx-footer-item">
+                                <a href="#" target="_blank" class="uk-text-decoration-none">行业资讯</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul class="uk-list yx-footer-list">
+                            <li class="yx-footer-item">
+                                <h4 class="uk-footer-title">服务支持</h4>
+                            </li>
+                            <li class="yx-footer-item">
+                                <a href="#" target="_blank" class="uk-text-decoration-none">售后政策</a>
+                            </li>
+                            <li class="yx-footer-item">
+                                <a href="#" target="_blank" class="uk-text-decoration-none">下载中心</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul class="uk-list yx-footer-list">
+                            <li class="yx-footer-item">
+                                <h4 class="uk-footer-title">产品中心</h4>
+                            </li>
+                            <li class="yx-footer-item">
+                                <a href="#" target="_blank" class="uk-text-decoration-none">工控一体机</a>
+                            </li>
+                            <li class="yx-footer-item">
+                                <a href="#" target="_blank" class="uk-text-decoration-none">工业触摸屏</a>
+                            </li>
+                            <li class="yx-footer-item">
+                                <a href="#" target="_blank" class="uk-text-decoration-none">商用广告机</a>
+                            </li>
+                            <li class="yx-footer-item">
+                                <a href="#" target="_blank" class="uk-text-decoration-none">工业平板电脑</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul class="uk-list yx-footer-list">
+                            <li class="yx-footer-item">
+                                <h4 class="uk-footer-title">解决方案</h4>
+                            </li>
+                            <li class="yx-footer-item">
+                                <a href="#" target="_blank" class="uk-text-decoration-none">智能制造</a>
+                            </li>
+                            <li class="yx-footer-item">
+                                <a href="#" target="_blank" class="uk-text-decoration-none">政务医疗</a>
+                            </li>
+                            <li class="yx-footer-item">
+                                <a href="#" target="_blank" class="uk-text-decoration-none">酒店餐饮</a>
+                            </li>
+                            <li class="yx-footer-item">
+                                <a href="#" target="_blank" class="uk-text-decoration-none">交通枢纽</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr class="yx-footer-hr uk-margin-top">
+        <div class="uk-text-center yx-footer-copyright">
+            <p class="uk-text-meta uk-margin-small-bottom" uk-scrollspy="cls: uk-animation-scale-up">Copyright © 2013~<?php echo date('Y'); ?> 深圳市研显触控科技有限公司 All Rights Reserved. <a class="uk-text-decoration-none" href="https://beian.miit.gov.cn/" target="_blank">粤ICP备2025399431号</a></p>
+        </div>
+    </div>
+</footer>
+<?php wp_footer(); ?>
 </body>
+
 </html>
