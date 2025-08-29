601d9f30fb87d0ef16f5bd059b14c54839710cd0
Kevin <wcdma020@hotmail.com>
2025-08-29 14:35:54 +0800

    chore：新增分类页面模板

---
+++ wp-content/themes/yanxian/category.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+<section class="uk-section">
+    <div class="uk-container uk-container-large">
+        <h2>分类页面</h2>
+        <h4>工控一体机产品</h4>
+    </div>
+</section>
+<?php get_footer();
+get_sidebar(); ?>
