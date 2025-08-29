9cd3f0b34c1609e5dc97115d24f7df11cd9ed8fa
Kevin <wcdma020@hotmail.com>
2025-08-29 17:36:51 +0800

    feat：添加面包屑区块样板

--- wp-content/themes/yanxian/category.php
+++ wp-content/themes/yanxian/category.php
@@ -1,9 +1,11 @@
 <?php get_header(); ?>
+<!-- 面包屑导航 Start -->
+<?php echo do_blocks('<!-- wp:pattern {"slug":"yanxian/breadcrumb"} /-->'); ?>
+<!-- 面包屑导航 End -->
 <section class="uk-section">
     <div class="uk-container uk-container-large">
         <h2>分类页面</h2>
         <h4>工控一体机产品</h4>
     </div>
 </section>
-<?php get_footer();
-get_sidebar(); ?>
+<?php get_footer(); get_sidebar(); ?>
