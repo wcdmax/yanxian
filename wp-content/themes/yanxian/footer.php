99859ca2ac0b434968324860ce9ddbc4fc855016
Kevin <wcdma020@hotmail.com>
2025-08-29 15:24:57 +0800

    chore：将footer中的静态信息改为使用get_option函数动态获取

--- wp-content/themes/yanxian/footer.php
+++ wp-content/themes/yanxian/footer.php
@@ -5,19 +5,19 @@
                 <ul class="uk-list yx-footer-list" uk-scrollspy="cls: uk-animation-slide-left; target: > li; delay: 100">
                     <li class="yx-footer-item">
                         <p>销售热线</p>
-                        <h4>19925438691</h4>
+                        <h4><?php echo get_option('company_phone', '19925438691'); ?></h4>
                     </li>
                     <li class="yx-footer-item">
                         <p>总机号码</p>
-                        <h4>400-8898-583</h4>
+                        <h4><?php echo get_option('company_400', '400-8898-583'); ?></h4>
                     </li>
                     <li class="yx-footer-item">
                         <p>邮箱地址</p>
-                        <h4>service@yxtouch.com</h4>
+                        <h4><?php echo get_option('company_email', 'service@yxtouch.com'); ?></h4>
                     </li>
                     <li class="yx-footer-item">
                         <p>公司地址</p>
-                        <p>深圳市宝安区沙井街道大王山社区大王山第三工业区A1栋六层</p>
+                        <p><?php echo get_option('company_address', '深圳市宝安区沙井街道大王山社区大王山第三工业区A1栋六层'); ?></p>
                     </li>
                 </ul>
             </div>
@@ -37,7 +37,7 @@
         </div>
         <hr class="yx-footer-hr uk-margin-top">
         <div class="uk-text-center yx-footer-copyright">
-            <p class="uk-text-meta uk-margin-small-bottom" uk-scrollspy="cls: uk-animation-scale-up">Copyright © 2013~<?php echo date('Y'); ?> 深圳市研显触控科技有限公司 All Rights Reserved. <a class="uk-text-decoration-none" href="https://beian.miit.gov.cn/" target="_blank">粤ICP备2025399431号</a></p>
+            <p class="uk-text-meta uk-margin-small-bottom" uk-scrollspy="cls: uk-animation-scale-up">Copyright © 2013~<?php echo date('Y'); ?> 深圳市研显触控科技有限公司 All Rights Reserved. <a class="uk-text-decoration-none" href="https://beian.miit.gov.cn/" target="_blank"><?php echo get_option('company_icp', '粤ICP备2025399431号'); ?></a></p>
         </div>
     </div>
 </footer>
