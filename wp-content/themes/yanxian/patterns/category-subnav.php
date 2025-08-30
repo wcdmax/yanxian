cf1ad7b92ad7f7f106897f46dd2fe84e81067755
Kevin <wcdma020@hotmail.com>
2025-08-30 11:52:17 +0800

    ui：为二级分类菜单添加滚动动画效果

--- wp-content/themes/yanxian/patterns/category-subnav.php
+++ wp-content/themes/yanxian/patterns/category-subnav.php
@@ -26,29 +26,29 @@ if (is_category()) :
     ]);
 ?>
 
-<!-- 二级分类菜单 Start -->
-<ul class="uk-subnav yx-subnav uk-margin-remove uk-subnav-divider">
-    <?php if (!empty($child_categories)) : ?>
-        <?php foreach ($child_categories as $child_cat) : ?>
-            <?php
-            // 判断是否为当前分类或当前分类的父级
-            $is_active = ($child_cat->term_id == $category->term_id) ||
-                in_array($child_cat->term_id, get_ancestors($category->term_id, 'category'));
-            ?>
-            <li<?php echo $is_active ? ' class="uk-active"' : ''; ?>>
-                <a href="<?php echo get_category_link($child_cat->term_id); ?>" <?php echo !$is_active ? ' class="uk-link-text"' : ''; ?>>
-                    <?php echo esc_html($child_cat->name); ?>
-                </a>
+    <!-- 二级分类菜单 Start -->
+    <ul class="uk-subnav yx-subnav uk-margin-remove uk-subnav-divider" uk-scrollspy="cls: uk-animation-slide-top-small; target: > li; delay: 100">
+        <?php if (!empty($child_categories)) : ?>
+            <?php foreach ($child_categories as $child_cat) : ?>
+                <?php
+                // 判断是否为当前分类或当前分类的父级
+                $is_active = ($child_cat->term_id == $category->term_id) ||
+                    in_array($child_cat->term_id, get_ancestors($category->term_id, 'category'));
+                ?>
+                <li<?php echo $is_active ? ' class="uk-active"' : ''; ?>>
+                    <a href="<?php echo get_category_link($child_cat->term_id); ?>" <?php echo !$is_active ? ' class="uk-link-text"' : ''; ?>>
+                        <?php echo esc_html($child_cat->name); ?>
+                    </a>
                 </li>
-            <?php endforeach; ?>
-        <?php else : ?>
-            <!-- 如果没有子分类，显示顶级分类本身 -->
-            <li class="uk-active">
-                <a href="<?php echo get_category_link($top_category->term_id); ?>">
-                    <?php echo esc_html($top_category->name); ?>
-                </a>
-            </li>
-        <?php endif; ?>
-</ul>
-<!-- 二级分类菜单 End -->
+                <?php endforeach; ?>
+            <?php else : ?>
+                <!-- 如果没有子分类，显示顶级分类本身 -->
+                <li class="uk-active">
+                    <a href="<?php echo get_category_link($top_category->term_id); ?>">
+                        <?php echo esc_html($top_category->name); ?>
+                    </a>
+                </li>
+            <?php endif; ?>
+    </ul>
+    <!-- 二级分类菜单 End -->
 <?php endif; ?>
