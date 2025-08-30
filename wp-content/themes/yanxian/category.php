7b0363a62bb70d8fb61509596d85662b21269050
Kevin <wcdma020@hotmail.com>
2025-08-30 10:55:48 +0800

    refact：重构分类页面，添加分类Banner和二级分类菜单，优化代码结构

--- wp-content/themes/yanxian/category.php
+++ wp-content/themes/yanxian/category.php
@@ -1,7 +1,47 @@
-<?php get_header(); ?>
-<!-- 面包屑导航 Start -->
-<?php echo do_blocks('<!-- wp:pattern {"slug":"yanxian/breadcrumb"} /-->'); ?>
-<!-- 面包屑导航 End -->
+<?php
+get_header();
+// 获取当前分类
+$category = get_queried_object();
+// 获取所属顶级分类
+$ancestors = get_ancestors($category->term_id, 'category');
+// 如果已经是顶级分类，则直接获取顶级分类的slug
+$top_category = empty($ancestors) ? $category : get_term(end($ancestors), 'category');
+$category_slug = $top_category->slug; // 获取顶级分类的slug
+$template = match (true) {
+    str_contains($category_slug, 'product') => 'product',
+    str_contains($category_slug, 'service') => 'download',
+    default => 'article'
+};
+?>
+<!-- 分类Banner Start -->
+<section class="uk-flex uk-section uk-flex-middle uk-height-large uk-background-cover" style="background-image: url('<?php echo get_theme_file_uri('/assets/images/banner.jpg'); ?>');">
+    <div class="uk-container uk-container-large">
+        <div uk-grid class="uk-grid-small uk-grid-match uk-width-expand uk-child-width-1-2">
+            <div>
+                <h2 class="yx-text-white"><?php echo single_cat_title(); ?></h2>
+                <p class="yx-text-white"><?php echo $category->description; ?></p>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- 分类Banner End -->
+<!-- 二级分类菜单 Start -->
+<section class="uk-section uk-padding-remove uk-box-shadow-small">
+    <div class="uk-container uk-container-large">
+        <div class="uk-flex uk-flex-middle uk-flex-between uk-padding-small uk-padding-remove-horizontal">
+            <ul class="uk-subnav yx-subnav uk-margin-remove uk-subnav-divider">
+                <li class="uk-active"><a href="#">工控一体机</a></li>
+                <li><a href="#" class="uk-link-text">工业触摸屏</a></li>
+                <li><a href="#">商用广告机</a></li>
+                <li><a href="#">工业平板电脑</a></li>
+            </ul>
+            <!-- 面包屑导航 Start -->
+            <?php echo do_blocks('<!-- wp:pattern {"slug":"yanxian/breadcrumb"} /-->'); ?>
+            <!-- 面包屑导航 End -->
+        </div>
+    </div>
+</section>
+<!-- 二级分类菜单 End -->
 <section class="uk-section">
     <div class="uk-container uk-container-large">
         <h2>分类页面</h2>
