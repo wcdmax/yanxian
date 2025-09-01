041134a18e646bf6fc03d3a3fcc150b40018a024
Kevin <wcdma020@hotmail.com>
2025-09-01 13:52:10 +0800

    ui：调整搜索输入框的宽度

--- wp-content/themes/yanxian/patterns/search.php
+++ wp-content/themes/yanxian/patterns/search.php
@@ -9,7 +9,7 @@
 $search_placeholder = get_query_var('search_placeholder', '搜索文章');
 ?>
 
-<form role="search" method="get" action="<?php echo home_url(); ?>" id="searchform" class="uk-search uk-search-default yx-sidebar-search">
+<form role="search" method="get" action="<?php echo home_url(); ?>" id="searchform" class="uk-search uk-width-expand uk-search-default yx-sidebar-search">
     <input name="s" type="search" aria-label="Search" placeholder="<?php echo $search_placeholder; ?> ..." class="uk-search-input uk-border-pill" onkeydown="return event.key != 'Enter' || this.value.trim() != ''">
     <button uk-search-icon class="uk-search-icon-flip" onclick="return this.previousElementSibling.value.trim() != ''"></button>
 </form>
