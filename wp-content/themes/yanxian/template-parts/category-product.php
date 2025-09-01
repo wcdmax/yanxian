e9375d0f12e2c9f0d85f7690badb916440d1e961
Kevin <wcdma020@hotmail.com>
2025-09-01 16:22:45 +0800

    chore：完善产品分类模板和相关图标

---
+++ wp-content/themes/yanxian/template-parts/category-product.php
@@ -0,0 +1,122 @@
+<div uk-grid class="uk-grid-small">
+    <div class="uk-width-1-4">
+        <div class="uk-margin">
+            <?php
+            set_query_var('search_placeholder', '搜索产品');
+            echo do_blocks('<!-- wp:pattern {"slug":"yanxian/search"} /-->');
+            ?>
+        </div>
+        <div class="uk-margin uk-margin-remove-top">
+            <?php echo do_blocks('<!-- wp:pattern {"slug":"yanxian/prod-filter"} /-->'); ?>
+        </div>
+        <div>
+            <?php echo do_blocks('<!-- wp:pattern {"slug":"yanxian/tags"} /-->'); ?>
+        </div>
+    </div>
+    <div class="uk-width-3-4">
+        <ul uk-grid class="uk-grid-small uk-grid-match yx-prod-list uk-child-width-1-3">
+            <li class="yx-prod-item">
+                <a target="_blank" title="研显G2工业触控一体机" href="#" class="uk-link-text uk-padding-small uk-border-rounded uk-box-shadow-small uk-text-decoration-none">
+                    <div class="yx-prod-img uk-overflow-hidden">
+                        <img alt="研显G2工业触控一体机" title="研显G2工业触控一体机" src="<?php echo get_theme_file_uri('/assets/images/g2.png') ?>">
+                    </div>
+                    <div class="yx-prod-info">
+                        <h4 class="uk-text-truncate">研显G2工业触控一体机</h4>
+                        <ul class="uk-subnav yx-prod-tags">
+                            <li><span>源头工厂</span></li>
+                            <li><span>现货速发</span></li>
+                            <li><span>支持定制</span></li>
+                            <li><span>可开专票</span></li>
+                        </ul>
+                        <p class="uk-text-meta text-clamp-2 text-clamp-ellipsis">友控G1工业触控一体机，支持内嵌、壁挂、桌面等多种安装方式。</p>
+                    </div>
+                </a>
+            </li>
+            <li class="yx-prod-item">
+                <a target="_blank" title="研显G2工业触控一体机" href="#" class="uk-link-text uk-padding-small uk-border-rounded uk-box-shadow-small uk-text-decoration-none">
+                    <div class="yx-prod-img uk-overflow-hidden">
+                        <img alt="研显G2工业触控一体机" title="研显G2工业触控一体机" src="<?php echo get_theme_file_uri('/assets/images/g2.png') ?>">
+                    </div>
+                    <div class="yx-prod-info">
+                        <h4 class="uk-text-truncate">研显G2工业触控一体机</h4>
+                        <ul class="uk-subnav yx-prod-tags">
+                            <li><span>源头工厂</span></li>
+                            <li><span>现货速发</span></li>
+                            <li><span>支持定制</span></li>
+                            <li><span>可开专票</span></li>
+                        </ul>
+                        <p class="uk-text-meta text-clamp-2 text-clamp-ellipsis">友控G1工业触控一体机，支持内嵌、壁挂、桌面等多种安装方式。</p>
+                    </div>
+                </a>
+            </li>
+            <li class="yx-prod-item">
+                <a target="_blank" title="研显G2工业触控一体机" href="#" class="uk-link-text uk-padding-small uk-border-rounded uk-box-shadow-small uk-text-decoration-none">
+                    <div class="yx-prod-img uk-overflow-hidden">
+                        <img alt="研显G2工业触控一体机" title="研显G2工业触控一体机" src="<?php echo get_theme_file_uri('/assets/images/g2.png') ?>">
+                    </div>
+                    <div class="yx-prod-info">
+                        <h4 class="uk-text-truncate">研显G2工业触控一体机</h4>
+                        <ul class="uk-subnav yx-prod-tags">
+                            <li><span>源头工厂</span></li>
+                            <li><span>现货速发</span></li>
+                            <li><span>支持定制</span></li>
+                            <li><span>可开专票</span></li>
+                        </ul>
+                        <p class="uk-text-meta text-clamp-2 text-clamp-ellipsis">友控G1工业触控一体机，支持内嵌、壁挂、桌面等多种安装方式。</p>
+                    </div>
+                </a>
+            </li>
+            <li class="yx-prod-item">
+                <a target="_blank" title="研显G2工业触控一体机" href="#" class="uk-link-text uk-padding-small uk-border-rounded uk-box-shadow-small uk-text-decoration-none">
+                    <div class="yx-prod-img uk-overflow-hidden">
+                        <img alt="研显G2工业触控一体机" title="研显G2工业触控一体机" src="<?php echo get_theme_file_uri('/assets/images/g2.png') ?>">
+                    </div>
+                    <div class="yx-prod-info">
+                        <h4 class="uk-text-truncate">研显G2工业触控一体机</h4>
+                        <ul class="uk-subnav yx-prod-tags">
+                            <li><span>源头工厂</span></li>
+                            <li><span>现货速发</span></li>
+                            <li><span>支持定制</span></li>
+                            <li><span>可开专票</span></li>
+                        </ul>
+                        <p class="uk-text-meta text-clamp-2 text-clamp-ellipsis">友控G1工业触控一体机，支持内嵌、壁挂、桌面等多种安装方式。</p>
+                    </div>
+                </a>
+            </li>
+            <li class="yx-prod-item">
+                <a target="_blank" title="研显G2工业触控一体机" href="#" class="uk-link-text uk-padding-small uk-border-rounded uk-box-shadow-small uk-text-decoration-none">
+                    <div class="yx-prod-img uk-overflow-hidden">
+                        <img alt="研显G2工业触控一体机" title="研显G2工业触控一体机" src="<?php echo get_theme_file_uri('/assets/images/g2.png') ?>">
+                    </div>
+                    <div class="yx-prod-info">
+                        <h4 class="uk-text-truncate">研显G2工业触控一体机</h4>
+                        <ul class="uk-subnav yx-prod-tags">
+                            <li><span>源头工厂</span></li>
+                            <li><span>现货速发</span></li>
+                            <li><span>支持定制</span></li>
+                            <li><span>可开专票</span></li>
+                        </ul>
+                        <p class="uk-text-meta text-clamp-2 text-clamp-ellipsis">友控G1工业触控一体机，支持内嵌、壁挂、桌面等多种安装方式。</p>
+                    </div>
+                </a>
+            </li>
+            <li class="yx-prod-item">
+                <a target="_blank" title="研显G2工业触控一体机" href="#" class="uk-link-text uk-padding-small uk-border-rounded uk-box-shadow-small uk-text-decoration-none">
+                    <div class="yx-prod-img uk-overflow-hidden">
+                        <img alt="研显G2工业触控一体机" title="研显G2工业触控一体机" src="<?php echo get_theme_file_uri('/assets/images/g2.png') ?>">
+                    </div>
+                    <div class="yx-prod-info">
+                        <h4 class="uk-text-truncate">研显G2工业触控一体机</h4>
+                        <ul class="uk-subnav yx-prod-tags">
+                            <li><span>源头工厂</span></li>
+                            <li><span>现货速发</span></li>
+                            <li><span>支持定制</span></li>
+                            <li><span>可开专票</span></li>
+                        </ul>
+                        <p class="uk-text-meta text-clamp-2 text-clamp-ellipsis">友控G1工业触控一体机，支持内嵌、壁挂、桌面等多种安装方式。</p>
+                    </div>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
