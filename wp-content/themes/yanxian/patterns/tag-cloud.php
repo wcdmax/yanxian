58afb6016ac66e2fae27f56cc26bc0743961a1ae
Kevin <wcdma020@hotmail.com>
2025-09-01 16:23:11 +0800

    feat：新增相关标签组件，展示与产品、分类、文章相关的热门标签

---
+++ wp-content/themes/yanxian/patterns/tag-cloud.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Title: 相关标签
+ * Slug: yanxian/tags
+ * Categories: yanxian, tags
+ * Description: 显示与产品、分类、文章相关的热门标签
+ */
+?>
+<div class="yx-prod-filter yx-sider-box uk-padding-small uk-border-rounded uk-box-shadow-small">
+    <h3><span uk-icon="icon: tag"></span>相关标签</h3>
+    <hr>
+    <ul class="yx-tags-list" uk-scrollspy="cls: uk-animation-scale-up; target: > li; delay: 100">
+        <li><a href="#" target="_blank">工业触控<span>（16）</span></a></li>
+        <li><a href="#" target="_blank">一体机<span>（12）</span></a></li>
+        <li><a href="#" target="_blank">触控屏<span>（8）</span></a></li>
+        <li><a href="#" target="_blank">教育行业<span>（5）</span></a></li>
+        <li><a href="#" target="_blank">会议平板<span>（3）</span></a></li>
+        <li><a href="#" target="_blank">智能办公<span>（2）</span></a></li>
+        <li><a href="#" target="_blank">触控一体机<span>（1）</span></a></li>
+        <li><a href="#" target="_blank">工业互联网<span>（1）</span></a></li>
+        <li><a href="#" target="_blank">数字化转型<span>（1）</span></a></li>
+        <li><a href="#" target="_blank">智慧教育<span>（1）</span></a></li>
+        <li><a href="#" target="_blank">物联网<span>（7）</span></a></li>
+        <li><a href="#" target="_blank">智能制造<span>（6）</span></a></li>
+        <li><a href="#" target="_blank">大数据<span>（5）</span></a></li>
+        <li><a href="#" target="_blank">人工智能<span>（4）</span></a></li>
+        <li><a href="#" target="_blank">智慧医疗<span>（3）</span></a></li>
+        <li><a href="#" target="_blank">云计算<span>（2）</span></a></li>
+        <li><a href="#" target="_blank">智能家居<span>（2）</span></a></li>
+        <li><a href="#" target="_blank">信息安全<span>（1）</span></a></li>
+        <li><a href="#" target="_blank">智慧城市<span>（1）</span></a></li>
+        <li><a href="#" target="_blank">区块链<span>（1）</span></a></li>
+    </ul>
+</div>
