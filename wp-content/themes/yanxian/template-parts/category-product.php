cf9eb8ad57d2c06c03b097a54ef861278e892ee6
Kevin <wcdma020@hotmail.com>
2025-09-01 16:46:37 +0800

    feat：在产品分类模板中添加分页区块

--- wp-content/themes/yanxian/template-parts/category-product.php
+++ wp-content/themes/yanxian/template-parts/category-product.php
@@ -120,3 +120,7 @@
         </ul>
     </div>
 </div>
+<hr>
+<div class="uk-flex uk-margin uk-flex-center">
+    <?php echo do_blocks('<!-- wp:pattern {"slug":"yanxian/pagination"} /-->'); ?>
+</div>
