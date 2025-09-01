0aa993086e32d4c7a5295abd0e0ebf96203c8549
Kevin <wcdma020@hotmail.com>
2025-09-01 17:15:52 +0800

    fix：修复可能存在的变量未定义问题

--- wp-content/themes/yanxian/patterns/pagination.php
+++ wp-content/themes/yanxian/patterns/pagination.php
@@ -7,7 +7,15 @@
  * Description: 显示一个列表分页，用户可以在不同的页面之间进行切换
  */
 
-$query = isset($args['query']) ? $args['query'] : $wp_query;
+global $wp_query;
+$custom_query = get_query_var('pagination_query');
+$query = $custom_query ? $custom_query : $wp_query;
+
+// 确保 query 对象存在
+if (!$query || !is_object($query)) {
+    return;
+}
+
 $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 
 $args = array(
@@ -17,13 +25,14 @@ $args = array(
 	'next_text' => '下一页',
 	'format' => '?paged=%#%',
 	'current' => max( 1, $paged ),
-	'total' => $query->max_num_pages,
+	'total' => isset($query->max_num_pages) ? $query->max_num_pages : 1,
 	'base' => str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
 );
 
 $links = paginate_links( $args );
 
-if( is_array( $links ) ) {
+// 只有当存在多页内容时才显示分页
+if( is_array( $links ) && isset($query->max_num_pages) && $query->max_num_pages > 1 ) {
 	echo '<nav aria-label="Pagination"><ul uk-margin class="uk-pagination yx-pagination" uk-scrollspy="cls: uk-animation-scale-up; target: > li; delay: 100;">';
 
 	// 遍历分页链接
