bf984907d3ff8f72eaf677ab9017062df245f8d3
Kevin <wcdma020@hotmail.com>
2025-09-01 13:37:32 +0800

    chore：新增搜索输入框样板

---
+++ wp-content/themes/yanxian/patterns/search.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Title: 搜索框
+ * Slug: yanxian/search
+ * Categories: yanxian, navigation，search
+ * Description: 显示一个搜索框，用户可以输入关键词进行搜索
+ */
+
+$search_placeholder = get_query_var('search_placeholder', '搜索文章');
+?>
+
+<form role="search" method="get" action="<?php echo home_url(); ?>" id="searchform" class="uk-search uk-search-default yx-sidebar-search">
+    <input name="s" type="search" aria-label="Search" placeholder="<?php echo $search_placeholder; ?> ..." class="uk-search-input uk-border-pill" onkeydown="return event.key != 'Enter' || this.value.trim() != ''">
+    <button uk-search-icon class="uk-search-icon-flip" onclick="return this.previousElementSibling.value.trim() != ''"></button>
+</form>
