d5e5b3c5caeb75cbe934a9f643696cb118c077f3
Kevin <wcdma020@hotmail.com>
2025-09-04 16:49:18 +0800

    chore：新增猜你喜欢侧边栏组件

---
+++ wp-content/themes/yanxian/patterns/suggest.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Title: 猜你喜欢
+ * Slug: yanxian/suggest
+ * Categories: yanxian, suggest
+ * Description: 根据用户浏览的内容，推荐相关的产品或文章
+ */
+?>
+
+<div class="yx-sider-box uk-padding-small uk-border-rounded uk-box-shadow-small">
+    <h3><span uk-icon="icon: tag"></span>猜你喜欢</h3>
+    <hr>
+    <ul class="ul-list yx-suggest-list" uk-scrollspy="target: > li; delay: 100; cls: uk-animation-slide-left-small;">
+        <li data-number="1" class="uk-flex uk-flex-middle yx-suggest-item">
+            <a target="_blank" href="#" class="text-clamp-2 text-clamp-ellipsis">研显深化国产芯片应用实践，强化工业一体机本土方案能力</a>
+        </li>
+        <li data-number="2" class="uk-flex uk-flex-middle yx-suggest-item">
+            <a target="_blank" href="#" class="text-clamp-2 text-clamp-ellipsis">数字化浪潮下，工控电脑一体机成物流仓库“效率担当”！</a>
+        </li>
+        <li data-number="3" class="uk-flex uk-flex-middle yx-suggest-item">
+            <a target="_blank" href="#" class="text-clamp-2 text-clamp-ellipsis">工业触摸屏闯进智能家居领域，普通屏幕只能“靠边站”？</a>
+        </li>
+        <li data-number="4" class="uk-flex uk-flex-middle yx-suggest-item">
+            <a target="_blank" href="#" class="text-clamp-2 text-clamp-ellipsis">工业4.0时代，研显触控如何用定制工控一体机适配不同行业场景？</a>
+        </li>
+        <li data-number="5" class="uk-flex uk-flex-middle yx-suggest-item">
+            <a target="_blank" href="#" class="text-clamp-2 text-clamp-ellipsis">研显解读——工业平板电脑厂家芯片选型中AMD被边缘化的根本原因</a>
+        </li>
+    </ul>
+</div>
