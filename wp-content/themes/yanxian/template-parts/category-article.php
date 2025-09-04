dc15b44911ffeb23b3bdd33e267ce1641e040c25
Kevin <wcdma020@hotmail.com>
2025-09-04 16:48:56 +0800

    chore：新增新闻资讯列表页及样式

---
+++ wp-content/themes/yanxian/template-parts/category-article.php
@@ -0,0 +1,181 @@
+<div uk-grid class="uk-grid-medium">
+    <div class="uk-width-3-4">
+        <ul class="uk-list yx-news-list">
+            <li>
+                <div uk-grid class="uk-grid-match uk-grid-match yx-news-list-item">
+                    <div class="uk-width-2-3">
+                        <div class="uk-flex uk-flex-column uk-flex-between">
+                            <a href="#" title="研显深化国产芯片应用实践，强化工业一体机本土方案能力" class="uk-link-text yx-news-list-title uk-text-decoration-none">
+                                <h3>研显深化国产芯片应用实践，强化工业一体机本土方案能力</h3>
+                                <p class="uk-text-meta text-clamp-3 text-clamp-ellipsis">在自动化工厂里，不光是机器人在干活，屏幕也没闲着。生产数据、设备状态、工艺流程，全靠一块好用的工业级显示器把这些信息准确稳定地展示出来。如果屏幕不稳定，操作员再熟练也没辙。现场节奏快、环境复杂，对显示...</p>
+                            </a>
+                            <div class="uk-flex uk-flex-middle yx-news-list-tags">
+                                <span uk-icon="icon: tag;ratio: 0.8" class="uk-icon"></span>
+                                <a href="#" rel="tag">工业显示器</a>
+                                <a href="#" rel="tag">工业级显示器</a>
+                                <a href="#" rel="tag">工业显示器厂家</a>
+                            </div>
+                            <div class="uk-flex uk-flex-middle uk-flex-between">
+                                <ul class="uk-subnav uk-subnav-divider yx-news-list-meta">
+                                    <li><a target="_blank" class="uk-link-reset" href="#"><span uk-icon="icon: list" class="uk-icon"></span>行业新闻</a></li>
+                                    <li><span uk-icon="icon: calendar" class="uk-icon"></span>2025-06-24</li>
+                                    <li><span uk-icon="icon: eye" class="uk-icon"></span>862</li>
+                                </ul>
+                                <span class="yx-news-list-arrow uk-icon" uk-icon="icon: arrow-right"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="uk-width-1-3">
+                        <div class="uk-border-rounded uk-overflow-hidden yx-news-list-litpic">
+                            <img uk-scrollspy="delay: 100; cls: uk-animation-scale-up" alt="研显深化国产芯片应用实践，强化工业一体机本土方案能力" title="研显深化国产芯片应用实践，强化工业一体机本土方案能力" src="//static.yxtouch.com/assets/images/news_1.jpg">
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div uk-grid class="uk-grid-match uk-grid-match yx-news-list-item">
+                    <div class="uk-width-2-3">
+                        <div class="uk-flex uk-flex-column uk-flex-between">
+                            <a href="#" title="研显解读——工业平板电脑厂家芯片选型中AMD被边缘化的根本原因" class="uk-link-text yx-news-list-title uk-text-decoration-none">
+                                <h3>研显解读——工业平板电脑厂家芯片选型中AMD被边缘化的根本原因</h3>
+                                <p class="uk-text-meta text-clamp-3 text-clamp-ellipsis">在自动化工厂里，不光是机器人在干活，屏幕也没闲着。生产数据、设备状态、工艺流程，全靠一块好用的工业级显示器把这些信息准确稳定地展示出来。如果屏幕不稳定，操作员再熟练也没辙。现场节奏快、环境复杂，对显示...</p>
+                            </a>
+                            <div class="uk-flex uk-flex-middle yx-news-list-tags">
+                                <span uk-icon="icon: tag;ratio: 0.8" class="uk-icon"></span>
+                                <a href="#" rel="tag">工业显示器</a>
+                                <a href="#" rel="tag">工业级显示器</a>
+                                <a href="#" rel="tag">工业显示器厂家</a>
+                            </div>
+                            <div class="uk-flex uk-flex-middle uk-flex-between">
+                                <ul class="uk-subnav uk-subnav-divider yx-news-list-meta">
+                                    <li><a target="_blank" class="uk-link-reset" href="#"><span uk-icon="icon: list" class="uk-icon"></span>行业新闻</a></li>
+                                    <li><span uk-icon="icon: calendar" class="uk-icon"></span>2025-06-24</li>
+                                    <li><span uk-icon="icon: eye" class="uk-icon"></span>862</li>
+                                </ul>
+                                <span class="yx-news-list-arrow uk-icon" uk-icon="icon: arrow-right"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="uk-width-1-3">
+                        <div class="uk-border-rounded uk-overflow-hidden yx-news-list-litpic">
+                            <img uk-scrollspy="delay: 100; cls: uk-animation-scale-up" alt="研显解读——工业平板电脑厂家芯片选型中AMD被边缘化的根本原因" title="研显解读——工业平板电脑厂家芯片选型中AMD被边缘化的根本原因" src="//static.yxtouch.com/assets/images/news_2.jpg">
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div uk-grid class="uk-grid-match uk-grid-match yx-news-list-item">
+                    <div class="uk-width-2-3">
+                        <div class="uk-flex uk-flex-column uk-flex-between">
+                            <a href="#" title="数字化浪潮下，工控电脑一体机成物流仓库“效率担当”！" class="uk-link-text yx-news-list-title uk-text-decoration-none">
+                                <h3>数字化浪潮下，工控电脑一体机成物流仓库“效率担当”！</h3>
+                                <p class="uk-text-meta text-clamp-3 text-clamp-ellipsis">在自动化工厂里，不光是机器人在干活，屏幕也没闲着。生产数据、设备状态、工艺流程，全靠一块好用的工业级显示器把这些信息准确稳定地展示出来。如果屏幕不稳定，操作员再熟练也没辙。现场节奏快、环境复杂，对显示...</p>
+                            </a>
+                            <div class="uk-flex uk-flex-middle yx-news-list-tags">
+                                <span uk-icon="icon: tag;ratio: 0.8" class="uk-icon"></span>
+                                <a href="#" rel="tag">工业显示器</a>
+                                <a href="#" rel="tag">工业级显示器</a>
+                                <a href="#" rel="tag">工业显示器厂家</a>
+                            </div>
+                            <div class="uk-flex uk-flex-middle uk-flex-between">
+                                <ul class="uk-subnav uk-subnav-divider yx-news-list-meta">
+                                    <li><a target="_blank" class="uk-link-reset" href="#"><span uk-icon="icon: list" class="uk-icon"></span>行业新闻</a></li>
+                                    <li><span uk-icon="icon: calendar" class="uk-icon"></span>2025-06-24</li>
+                                    <li><span uk-icon="icon: eye" class="uk-icon"></span>862</li>
+                                </ul>
+                                <span class="yx-news-list-arrow uk-icon" uk-icon="icon: arrow-right"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="uk-width-1-3">
+                        <div class="uk-border-rounded uk-overflow-hidden yx-news-list-litpic">
+                            <img uk-scrollspy="delay: 100; cls: uk-animation-scale-up" alt="数字化浪潮下，工控电脑一体机成物流仓库“效率担当”！" title="数字化浪潮下，工控电脑一体机成物流仓库“效率担当”！" src="//static.yxtouch.com/assets/images/news_3.jpg">
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div uk-grid class="uk-grid-match uk-grid-match yx-news-list-item">
+                    <div class="uk-width-2-3">
+                        <div class="uk-flex uk-flex-column uk-flex-between">
+                            <a href="#" title="工业4.0时代，研显触控如何用定制工控一体机适配不同行业场景？" class="uk-link-text yx-news-list-title uk-text-decoration-none">
+                                <h3>工业4.0时代，研显触控如何用定制工控一体机适配不同行业场景？</h3>
+                                <p class="uk-text-meta text-clamp-3 text-clamp-ellipsis">在自动化工厂里，不光是机器人在干活，屏幕也没闲着。生产数据、设备状态、工艺流程，全靠一块好用的工业级显示器把这些信息准确稳定地展示出来。如果屏幕不稳定，操作员再熟练也没辙。现场节奏快、环境复杂，对显示...</p>
+                            </a>
+                            <div class="uk-flex uk-flex-middle yx-news-list-tags">
+                                <span uk-icon="icon: tag;ratio: 0.8" class="uk-icon"></span>
+                                <a href="#" rel="tag">工业显示器</a>
+                                <a href="#" rel="tag">工业级显示器</a>
+                                <a href="#" rel="tag">工业显示器厂家</a>
+                            </div>
+                            <div class="uk-flex uk-flex-middle uk-flex-between">
+                                <ul class="uk-subnav uk-subnav-divider yx-news-list-meta">
+                                    <li><a target="_blank" class="uk-link-reset" href="#"><span uk-icon="icon: list" class="uk-icon"></span>行业新闻</a></li>
+                                    <li><span uk-icon="icon: calendar" class="uk-icon"></span>2025-06-24</li>
+                                    <li><span uk-icon="icon: eye" class="uk-icon"></span>862</li>
+                                </ul>
+                                <span class="yx-news-list-arrow uk-icon" uk-icon="icon: arrow-right"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="uk-width-1-3">
+                        <div class="uk-border-rounded uk-overflow-hidden yx-news-list-litpic">
+                            <img uk-scrollspy="delay: 100; cls: uk-animation-scale-up" alt="工业4.0时代，研显触控如何用定制工控一体机适配不同行业场景？" title="工业4.0时代，研显触控如何用定制工控一体机适配不同行业场景？" src="//static.yxtouch.com/assets/images/news_4.jpg">
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div uk-grid class="uk-grid-match uk-grid-match yx-news-list-item">
+                    <div class="uk-width-2-3">
+                        <div class="uk-flex uk-flex-column uk-flex-between">
+                            <a href="#" title="工业触摸屏闯进智能家居领域，普通屏幕只能“靠边站”？" class="uk-link-text yx-news-list-title uk-text-decoration-none">
+                                <h3>工业触摸屏闯进智能家居领域，普通屏幕只能“靠边站”？</h3>
+                                <p class="uk-text-meta text-clamp-3 text-clamp-ellipsis">在自动化工厂里，不光是机器人在干活，屏幕也没闲着。生产数据、设备状态、工艺流程，全靠一块好用的工业级显示器把这些信息准确稳定地展示出来。如果屏幕不稳定，操作员再熟练也没辙。现场节奏快、环境复杂，对显示...</p>
+                            </a>
+                            <div class="uk-flex uk-flex-middle yx-news-list-tags">
+                                <span uk-icon="icon: tag;ratio: 0.8" class="uk-icon"></span>
+                                <a href="#" rel="tag">工业显示器</a>
+                                <a href="#" rel="tag">工业级显示器</a>
+                                <a href="#" rel="tag">工业显示器厂家</a>
+                            </div>
+                            <div class="uk-flex uk-flex-middle uk-flex-between">
+                                <ul class="uk-subnav uk-subnav-divider yx-news-list-meta">
+                                    <li><a target="_blank" class="uk-link-reset" href="#"><span uk-icon="icon: list" class="uk-icon"></span>行业新闻</a></li>
+                                    <li><span uk-icon="icon: calendar" class="uk-icon"></span>2025-06-24</li>
+                                    <li><span uk-icon="icon: eye" class="uk-icon"></span>862</li>
+                                </ul>
+                                <span class="yx-news-list-arrow uk-icon" uk-icon="icon: arrow-right"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="uk-width-1-3">
+                        <div class="uk-border-rounded uk-overflow-hidden yx-news-list-litpic">
+                            <img uk-scrollspy="delay: 100; cls: uk-animation-scale-up" alt="工业触摸屏闯进智能家居领域，普通屏幕只能“靠边站”？" title="工业触摸屏闯进智能家居领域，普通屏幕只能“靠边站”？" src="//static.yxtouch.com/assets/images/news_5.jpg">
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="uk-width-1-4">
+        <div uk-grid class="uk-grid-small uk-child-width-1-1">
+            <div>
+                <?php
+                set_query_var('search_placeholder', '搜索文章');
+                echo do_blocks('<!-- wp:pattern {"slug":"yanxian/search"} /-->');
+                ?>
+            </div>
+            <div>
+                <?php echo do_blocks('<!-- wp:pattern {"slug":"yanxian/tags"} /-->'); ?>
+            </div>
+            <div>
+                <?php echo do_blocks('<!-- wp:pattern {"slug":"yanxian/suggest"} /-->'); ?>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="uk-flex uk-margin uk-flex-center">
+    <hr>
+    <?php echo do_blocks('<!-- wp:pattern {"slug":"yanxian/pagination"} /-->'); ?>
+</div>
